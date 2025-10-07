77e9f78ed60bd5b059758e5050680954491067f6
fabricio-vianna <fab.vianna.1976@gmail.com>
2025-10-07 11:50:57 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,24 @@
 package application;
 
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
     public static void main(String[] args) {
 
-        Department dp = new Department(1, "Books");
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 
-        System.out.println(dp);
+        Department obj = new Department(1, "Books");
+
+        Seller seller = new Seller(21, "Joao", "joao@gmail.com", LocalDate.parse("09/09/2009", dtf), 2500.0, obj);
+
+
+        System.out.println(obj);
+        System.out.println(seller);
     }
 }
