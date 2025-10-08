15cff9c52d772e3831adcccd2f5b03d90006d5ee
fabricio-vianna <fab.vianna.1976@gmail.com>
2025-10-08 12:31:07 -0300

    Reusing instantiation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,29 +1,17 @@
 package application;
 
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
 
     public static void main(String[] args) {
 
-        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-
-        Department obj = new Department(1, "Books");
-
-        Seller seller = new Seller(21, "Joao", "joao@gmail.com", LocalDate.parse("09/09/2009", dtf), 2500.0, obj);
-
         SellerDao sellerDao = DaoFactory.createSellerDao();
 
-        Seller seller1 = sellerDao.findById(3);
-
-        System.out.println(obj);
+        System.out.println("==== TEST 1: seller findById ====");
+        Seller seller = sellerDao.findById(3);
         System.out.println(seller);
-        System.out.println(seller1);
     }
 }
