e0dd9b1b7da8057738775368a2efc4295bf57684
fabricio-vianna <fab.vianna.1976@gmail.com>
2025-10-10 18:54:18 -0300

    DepartmentDaoJDBC and DaoFactory

--- src/model/dao/impl/DepartmentDaoJDBC.java
+++ src/model/dao/impl/DepartmentDaoJDBC.java
@@ -1,4 +1,42 @@
 package model.dao.impl;
 
-public class DepartmentDaoJDBC {
+import java.sql.Connection;
+import java.util.List;
+
+import model.dao.DepartmentDao;
+import model.entities.Department;
+
+public class DepartmentDaoJDBC implements DepartmentDao {
+
+    private Connection conn;
+
+    public DepartmentDaoJDBC(Connection conn) {
+        this.conn = conn;
+    }
+
+
+    @Override
+    public void insert(Department obj) {
+
+    }
+
+    @Override
+    public void update(Department obj) {
+
+    }
+
+    @Override
+    public void deleteById(Integer id) {
+
+    }
+
+    @Override
+    public Department findById(Integer id) {
+        return null;
+    }
+
+    @Override
+    public List<Department> findAll() {
+        return List.of();
+    }
 }
